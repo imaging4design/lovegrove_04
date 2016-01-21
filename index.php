604a9f17c0a3ea7034e6d99553da5a3c887f9b25
Gavin Lovegrove <gavin@imaging4design.co.nz>
2016-01-21 19:50:41 +1300

    progress to date

--- index.php
+++ index.php
@@ -7,15 +7,29 @@
 
 			<div class="row">
 				<div class="col-sm-12">
-					<h1 class="page-title">CSS Animations</h1>
-					<p>Sliding mobile phone screens</p>
+					<h1 class="page-title">Website Design &amp; Development</h1>
+
+					<h3>The challenge of today's modern website</h3>
+
+					<p>
+						Creating a new website today takes a slight mental adjustment. We now have to think of all the different platforms your site might be viewed from. Once apon a time it was simply the humble desktop, now days your audience will more than likely be finding you from a phone or tablet device. So what does this mean? Take your current website for example, if it's been around for a few years, its likely to be a fixed width site that gets shrunk down when viewed on a mobile device. This means a lot of frustrating zoom in / zoom out behaviour for your viewers - far from ideal! 
+					</p>
 				</div><!--ENDS col-->
 			</div><!--ENDS row-->
 
 
-			<div class="row row-no-padding">
+			<div class="row">
 
-				<div class="col-sm-6">
+				<div class="col-sm-8">
+
+					<img src="img/web/web_anz.png" class="img-responsive" style="margin:80px 0 60px 0;">
+
+					<h3 class="text-center">Athletics NZ Records and Rankings</h3>
+					<p class="text-center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+				
+				</div><!--ENDS col-->
+
+				<div class="col-sm-4">
 
 					<div id="carousel01" class="carousel slide" data-ride="carousel" data-interval="false">
 
@@ -31,60 +45,60 @@
 
 							<div class="item active">
 								<div class="slide-up"></div>
-								<a href="#carousel01" data-slide="next"><img src="img/web/iPhone2.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
+								<a href="websites#carousel01" data-slide="next"><img src="img/web/iPhone_anz1.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
 								<div class="carousel-caption">
-									<p><strong>HOME PAGE</strong> - displays latest site statistics, news items, recent NZ records, athlete birthdays, Today in History and year's performance leaders.</p>
+									<p><strong>HOME</strong> - displays latest site statistics, news items, recent NZ records, athlete birthdays, today in history and year's performance leaders.</p>
 								</div>
 							</div>
 
 							<div class="item">
 								<div class="slide-up"></div>
-								<a href="#carousel01" data-slide="next"><img src="img/web/iPhone3.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
+								<a href="websites#carousel01" data-slide="next"><img src="img/web/iPhone_anz2.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
 								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+									<p><strong>SEARCH LISTS</strong> - search by current year or all-time lists, all events, genders and age groups.</p>
 								</div>
 							</div>
 
 							<div class="item">
 								<div class="slide-up"></div>
-								<a href="#carousel01" data-slide="next"><img src="img/web/iPhone4.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
+								<a href="websites#carousel01" data-slide="next"><img src="img/web/iPhone_anz3.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
 								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+									<p><strong>SEARCH RESULTS</strong> - displays full lists of performances per event with collapsable tables designed for mobile use.</p>
 								</div>
 							</div>
 
 							<div class="item">
 								<div class="slide-up"></div>
-								<a href="#carousel01" data-slide="next"><img src="img/web/iPhone5.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
+								<a href="websites#carousel01" data-slide="next"><img src="img/web/iPhone_anz4.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
 								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+									<p><strong>ATHLETE PROFILES</strong> - shows both mini and full profiles for each athlete including NZ representaion, medals won and annual progressions.</p>
 								</div>
 							</div>
 
 							<div class="item">
 								<div class="slide-up"></div>
-								<a href="#carousel01" data-slide="next"><img src="img/web/iPhone6.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
+								<a href="websites#carousel01" data-slide="next"><img src="img/web/iPhone_anz5.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
 								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+									<p><strong>NZ RECORDS</strong> - displays New Zealand records for all age groups and genders, NZ National, NZ Resident and All Commers records.</p>
 								</div>
 							</div>
 
 							<div class="item">
 								<div class="slide-up"></div>
-								<a href="#carousel01" data-slide="next"><img src="img/web/iPhone7.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
+								<a href="websites#carousel01" data-slide="next"><img src="img/web/iPhone_anz6.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
 								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+									<p><strong>ATHLETE SEARCH</strong> - search for an individual athlete to display their full competitive and results history.</p>
 								</div>
 							</div>
 								
 						</div>
 
 						<!-- Controls -->
-						<a class="left carousel-control" href="#carousel01" role="button" data-slide="prev">
+						<a class="left carousel-control" href="websites#carousel01" role="button" data-slide="prev">
 							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 							<span class="sr-only">Previous</span>
 						</a>
-						<a class="right carousel-control" href="#carousel01" role="button" data-slide="next">
+						<a class="right carousel-control" href="websites#carousel01" role="button" data-slide="next">
 							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 							<span class="sr-only">Next</span>
 						</a>
@@ -92,95 +106,6 @@
 					</div><!-- ENDS carousel-example-generic -->
 
 					<h3 class="text-center">Athletics NZ Records and Rankings</h3>
-					<p class="text-center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-				
-				</div><!--ENDS col-->
-
-				<div class="col-sm-6">
-
-					<div id="carousel02" class="carousel slide" data-ride="carousel" data-interval="false">
-
-						<!-- Indicators 
-						<ol class="carousel-indicators">
-							<li data-target="#carousel02" data-slide-to="0" class="active"></li>
-							<li data-target="#carousel02" data-slide-to="1"></li>
-							<li data-target="#carousel02" data-slide-to="2"></li>
-						</ol>-->
-
-						<!-- Wrapper for slides -->
-						<div class="carousel-inner" role="listbox">
-
-							<div class="item active">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn1.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p><strong>HOME PAGE</strong> - displays latest site statistics, news items, recent NZ records, athlete birthdays, Today in History and year's performance leaders.</p>
-								</div>
-							</div>
-
-							<div class="item">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn2.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-								</div>
-							</div>
-
-							<div class="item">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn3.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-								</div>
-							</div>
-
-							<div class="item">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn4.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-								</div>
-							</div>
-
-							<div class="item">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn5.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-								</div>
-							</div>
-
-							<div class="item">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn6.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-								</div>
-							</div>
-
-							<div class="item">
-								<div class="slide-up"></div>
-								<a href="#carousel02" data-slide="next"><img src="img/web/iPhone_elearn7.png" width="320" height="600" alt="..." class="center-block img-responsive"></a>
-								<div class="carousel-caption">
-									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-								</div>
-							</div>
-								
-						</div>
-
-						<!-- Controls -->
-						<a class="left carousel-control" href="#carousel02" role="button" data-slide="prev">
-							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#carousel02" role="button" data-slide="next">
-							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
-
-					</div><!-- ENDS carousel-example-generic -->
-
-					<h3 class="text-center">eLearn Economics</h3>
 					<p class="text-center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
 				
 				</div><!--ENDS col-->
--- pages/inc/graphics.php
+++ pages/inc/graphics.php
@@ -1,12 +1,12 @@
 <div class="row">
 	<div class="col-sm-12 outline">
-		<h3>Graphics View the case studies ...</h3>
+		<h3>View the case studies ...</h3>
 	</div><!--ENDS col-->
 </div><!--ENDS row-->
 
 <div class="row">
 	<div class="col-sm-4 outline">
-		<img src="<?php echo $site_path; ?>img/web/web_browser2.png" class="img-responsive">
+		<img src="<?php echo $site_path; ?>img/web/web_anz.png" class="img-responsive">
 	</div><!--ENDS col-->
 	<div class="col-sm-8 outline">
 		<h4>Track &amp; Field Statistics</h4>
@@ -19,7 +19,7 @@
 
 <div class="row">
 	<div class="col-sm-4 outline">
-		<img src="<?php echo $site_path; ?>img/web/web_browser3.png" class="img-responsive">
+		<img src="<?php echo $site_path; ?>img/web/web_timesheet.png" class="img-responsive">
 	</div><!--ENDS col-->
 	<div class="col-sm-8 outline">
 		<h4>Athletics NZ Rankings</h4>
@@ -32,7 +32,7 @@
 
 <div class="row">
 	<div class="col-sm-4 outline">
-		<img src="<?php echo $site_path; ?>img/web/web_browser9.png" class="img-responsive">
+		<img src="<?php echo $site_path; ?>img/web/web_track_stats.png" class="img-responsive">
 	</div><!--ENDS col-->
 	<div class="col-sm-8 outline">
 		<h4>Job &amp; Timesheet Manager</h4>
